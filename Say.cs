2081e1917e2c65a3adc6b118bcc59432dccb19a9
tarathep <bokie.tarathep@gmail.com>
2022-04-22 11:16:04 +0700

    first commit

---
+++ Say.cs
@@ -0,0 +1,10 @@
+﻿namespace HelloWorld
+{
+    public class Say
+    {
+        public string Hello(string message)
+        {
+            return message+" from Hello World lib";
+        }
+    }
+}
